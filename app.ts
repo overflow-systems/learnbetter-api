f2898be5a3c67427e5b89e97164ae51f5d6ab69b
Bruno Brasolin <brunobrasolinc@gmail.com>
2022-04-26 21:40:15 -0300

    fix app

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import rotas from './source/rotas';
-
+require('dotenv/config');
 
 const app = express();
 
@@ -8,4 +8,4 @@ app.use(express.json());
 
 app.use(rotas);
 
-app.listen(3333, () => console.log('Porta 3333'));
+app.listen(process.env.PORT || 3333, () => console.log('Porta 3333'));
